332f1a77b264a36fa3ebbda1f2dbb76871ff3aec
brandonmendoza55 <40575392+brandonmendoza55@users.noreply.github.com>
2018-06-27 12:11:40 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-import pygame
+import sys, pygame
+from pygame.locals import*
+ALTURA = 300
+ANCHO = 300
 
 
-pygame.init()
+def main():
+	pygame.init()	
+	screen = pygame.display.set_mode((ALTURA,ANCHO))
+	pygame.display.set_caption("welcome to pygame")
